dec4a16bdc6738ba9f7e7ba18aecd2f716c5a6fd
Nate C-K <njc@euclidean3.space>
2023-04-24 06:51:28 -0500

    Arguments to gen script

--- make_flashcards.py
+++ make_flashcards.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys, os, os.path
 import csv
 
 CSV_PATH = "fred-jehle-spanish-verbs-master/jehle_verb_database.csv"
@@ -20,18 +21,37 @@ FORMS = [
         ]
 
 def main():
+    args = sys.argv[1:]
+    if len(args) == 2:
+        mood, tense = args
+    elif len(args) == 3:
+        mood, tense, ending = args
+    else:
+        print("Invalid arguments. USAGE: make_flashcards.py <mood> <tense> [<ending>]")
     # Encoding needs to remove Unicode BOM
     with open(CSV_PATH, encoding="utf-8-sig") as f:
         reader = csv.DictReader(f)
         rows = [ row for row in reader ]
     #print(rows[0])
-    ar_verbs = [ row for row in rows if row["infinitive"].endswith("ar") ]
+    validate_args(rows, mood, tense, ending)
+    selected_verbs = select_verbs(rows, mood, tense, ending)
+    write_output(selected_verbs)
+
+def select_verbs(rows, mood, tense, ending):
+    selected_verbs = rows
+    if ending:
+        selected_verbs = [
+                row for row in selected_verbs
+                if row["infinitive"].endswith(ending)
+                ]
     selected_verbs = [
-            row for row in rows
-            if row["infinitive"].endswith("ar")
-            and row["mood"] == "Indicativo"
-            and row["tense"] == "Presente"
+            row for row in selected_verbs
+            if row["mood"] == mood
+            and row["tense"] == tense
             ]
+    return selected_verbs
+
+def write_output(selected_verbs):
     print("#separator:Semicolon")
     print("#html:false")
     for row in selected_verbs:
@@ -44,6 +64,22 @@ def main():
                     + pronoun + " " + row["form_"+form]
                     + '"'
                     )
+
+def validate_args(rows, mood, tense, ending):
+    err = sys.stderr
+    all_moods = set(( row["mood"] for row in rows ))
+    all_tenses = set(( row["tense"] for row in rows ))
+    if mood not in all_moods:
+        print("Invalid mood: " + mood, file=err)
+        print("Available moods: " + (', '.join(all_moods)), file=err)
+        sys.exit(1)
+    if tense not in all_tenses:
+        print("Invalid tense: " + tense, file=err)
+        print("Available tenses: " + (', '.join(all_tenses)), file=err)
+        sys.exit(1)
+    if ending and ending not in ['ar', 'er', 'ir']:
+        print("Invalid ending: " + ending, file=err)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
